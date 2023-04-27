700ff2a87990d28ba9d04db3d58aac44c4369472
Artur Makaryuk <art.makaryuk@gmail.com>
2023-04-27 18:51:21 +0700

    home

--- src/main/java/crudproject/service/UserServiceImpl.java
+++ src/main/java/crudproject/service/UserServiceImpl.java
@@ -2,7 +2,6 @@ package crudproject.service;
 
 import crudproject.dao.UserDao;
 import crudproject.model.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
